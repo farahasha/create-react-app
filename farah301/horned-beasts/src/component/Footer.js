29bda134fbda68c1ff8d1cf4056922748915db98
farahasha <farahasha93@yahoo.com>
2021-09-06 00:44:48 +0300

    add

---
+++ farah301/horned-beasts/src/component/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+     <footer>
+    
+     <h5>
+  farah asha
+     </h5>
+    
+     </footer>
+     
+     
+    )
+  }
+}
+
+export default Footer;
